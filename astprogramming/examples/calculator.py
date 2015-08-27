18a480f139670dd0762a73258888dbd9ffbbd9af
Spencer Pearson <speeze.pearson@gmail.com>
2015-08-27 12:28:41 -0700

    added Variable, Assignment to calculator

--- astprogramming/examples/calculator.py
+++ astprogramming/examples/calculator.py
@@ -16,3 +16,19 @@ class Add(astprogramming.ASTNode):
 
 astprogramming.operators.Pythonizer.set_method(Add, lambda self, node: ast.BinOp(op=ast.Add(), left=self.call(node.left), right=self.call(node.right)))
 astprogramming.operators.PseudocodeGenerator.set_method(Add, lambda self, node: '{} + {}'.format(self.call(node.left), self.call(node.right)))
+
+class Variable(astprogramming.ASTNode):
+  attribute_names = ('name',)
+  def __init__(self, name, **kwargs):
+    super().__init__(name=name, **kwargs)
+
+astprogramming.operators.Pythonizer.set_method(Variable, lambda self, node: ast.Name(id=node.name))
+astprogramming.operators.PseudocodeGenerator.set_method(Variable, lambda self, node: node.name)
+
+class Assignment(astprogramming.ASTNode):
+  child_names = ('lhs', 'value')
+  def __init__(self, lhs, value, **kwargs):
+    super().__init__(lhs=lhs, value=value, **kwargs)
+
+astprogramming.operators.Pythonizer.set_method(Assignment, lambda self, node: ast.Assign(targets=[self.call(node.lhs)], value=self.call(node.value)))
+astprogramming.operators.PseudocodeGenerator.set_method(Assignment, lambda self, node: '{} = {}'.format(self.call(node.lhs), self.call(node.value)))
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,12 +1,26 @@
 import unittest
+import ast
 from astprogramming.operators import Pythonizer, PseudocodeGenerator
 from astprogramming.examples.calculator import *
 
 class TestCalculator(unittest.TestCase):
-  def test_evaluation(self):
+  def test_number(self):
     self.assertEqual(1, Pythonizer.evaluate(Number(1)))
-    self.assertEqual(3, Pythonizer.evaluate(Add(Number(1), Number(2))))
-
-  def test_pseudocode(self):
     self.assertEqual('1', PseudocodeGenerator().call(Number(1)))
+
+  def test_add(self):
+    self.assertEqual(3, Pythonizer.evaluate(Add(Number(1), Number(2))))
     self.assertEqual('1 + 2', PseudocodeGenerator().call(Add(Number(1), Number(2))))
+
+  def test_variable(self):
+    python_ast = Pythonizer().call(Variable('x'))
+    self.assertIsInstance(python_ast, ast.Name)
+    self.assertEqual('x', python_ast.id)
+    self.assertEqual('x', PseudocodeGenerator().call(Variable('x')))
+
+  def test_assignment(self):
+    python_ast = Pythonizer().call(Assignment(Variable('x'), Number(1)))
+    self.assertIsInstance(python_ast, ast.Assign)
+    self.assertEqual('x', python_ast.targets[0].id)
+    self.assertEqual(1, python_ast.value.n)
+    self.assertEqual('x = 1', PseudocodeGenerator().call(Assignment(Variable('x'), Number(1))))
